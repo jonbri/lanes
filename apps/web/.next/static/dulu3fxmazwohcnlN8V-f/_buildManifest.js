7b2fc93338c26ee5fd78e064727d7aba75b5193d
Jonathan Brink <jonathandavidbrink@gmail.com>
2023-02-09 14:49:45 -0500

    build

--- apps/web/.next/static/dulu3fxmazwohcnlN8V-f/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-058f82d8834fe5ad.js"],"/_error":["static/chunks/pages/_error-16e423f1806eadc7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
